c798eaa8164ad63898587c77ff5521952fbab2ac
Mike Harley <michael.harley@slrconsulting.com>
2025-01-21 15:57:40 +0000

    connection to mongodb established

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,5 +1,29 @@
 const getAllTasks = (req, res) =>{
-    res.send('all items from the file')
+    res.send('get all tasks')
 }
 
-module.exports = { getAllTasks }
+const createTask = (req, res)=>{
+    res.json(req.body)
+}
+
+const getTask = (req, res)=>{
+    res.json({ id: req.params.id })
+}
+
+const updateTask = (req, res)=>{
+    res.send('Update task')
+}
+
+const deleteTask = (req, res)=>{
+    res.send('Delete task')
+}
+
+
+
+module.exports = { 
+    getAllTasks,
+    getTask,
+    createTask,
+    updateTask,
+    deleteTask
+ }
